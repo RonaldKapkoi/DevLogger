dcfd6cdde59f7855a89f8943a69632385c6b0f54
RonaldKapkoi <kibiwottkapkoi@gmail.com>
2025-02-19 06:07:23 -0500

    changes

--- server/src/server.js
+++ server/src/server.js
@@ -1,13 +1,23 @@
 import express, { json } from "express";
 import "dotenv/config";
+import { getHome } from "./controllers/home.js";
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.json({
-    message: "GET / request",
-  });
-});
+app.get("/", getHome);
+
+// create a student record
+app.post("/students", createStudents);
+
+// Read students data
+app.get("/students", getStudents);
+
+// Update student records
+app.update("/students", updateStudent);
+
+// Delete student records
+app.delete("/students", deleteStudents);
+
 app.listen(process.env.PORT, () => {
   console.log(`Sever is running on port ${process.env.PORT}`);
 });
